c96a3e8ba1697b0975e6e71f9d15d070372c956f
Aram Hăvărneanu <aram@mgk.ro>
2024-03-05 15:38:34 +0100

    add package declarations
    Signed-off-by: Aram Hăvărneanu <aram@mgk.ro>

--- org/cuelang/cue/Build.java
+++ org/cuelang/cue/Build.java
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+package org.cuelang.cue;
+
 public final class Build {
     public record FileName(String name) implements BuildOption {}
 
--- org/cuelang/cue/Eval.java
+++ org/cuelang/cue/Eval.java
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+package org.cuelang.cue;
+
 public final class Eval {
     public record All() implements EvalOption {}
 
