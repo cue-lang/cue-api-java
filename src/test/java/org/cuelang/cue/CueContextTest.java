a7c5000e6f0651c00fe3cae38ef44e26ad3832de
Aram Hăvărneanu <aram@mgk.ro>
2024-03-19 15:05:22 +0100

    add tests for CueContext
    We merely test that we can create CUE values without exceptions,
    we don't test their properties just yet. That will come later.
    
    Signed-off-by: Aram Hăvărneanu <aram@mgk.ro>

--- src/test/java/org/cuelang/cue/CueContextTest.java
+++ src/test/java/org/cuelang/cue/CueContextTest.java
@@ -15,5 +15,86 @@ class CueContextTest {
 
     @Test
     void top() {
+        ctx.top();
+    }
+
+    @Test
+    void bottom() {
+        ctx.bottom();
+    }
+
+    @Test
+    void compileEmpty() {
+        assertDoesNotThrow(() -> {
+            ctx.compile("");
+        });
+    }
+
+    @Test
+    void compileEmptyWithOptions() {
+        assertDoesNotThrow(() -> {
+            ctx.compile("", new Build.FileName("empty.cue"), new Build.ImportPath("example.com/foo/bar"));
+        });
+    }
+
+    @Test
+    void compileScalarWithOptions() {
+        assertDoesNotThrow(() -> {
+           ctx.compile("int", new Build.FileName("empty.cue"), new Build.ImportPath("example.com/foo/bar"));
+        });
+    }
+
+    @Test
+    void compileEmptyBytes() {
+        byte[] buf = {};
+        assertDoesNotThrow(() -> {
+            ctx.compile(buf);
+        });
+    }
+
+    @Test
+    void compileEmptyBytesWithOptions() {
+        byte[] buf = {};
+        assertDoesNotThrow(() -> {
+            ctx.compile(buf, new Build.FileName("empty.cue"), new Build.ImportPath("example.com/foo/bar"));
+        });
+    }
+
+    @Test
+    void compileBytesWithOptions() {
+        assertDoesNotThrow(() -> {
+           ctx.compile("int".getBytes(), new Build.FileName("empty.cue"), new Build.ImportPath("example.com/foo/bar"));
+        });
+    }
+
+    @Test
+    void toValueFromLong() {
+        ctx.toValue(-1);
+    }
+
+    @Test
+    void toValueAsUnsigned() {
+        ctx.toValueAsUnsigned(0xcafebabeL);
+    }
+
+    @Test
+    void toValueFromBoolean() {
+        ctx.toValue(true);
+    }
+
+    @Test
+    void toValueFromDouble() {
+        ctx.toValue(0.123);
+    }
+
+    @Test
+    void toValueFromString() {
+        ctx.toValue("hello");
+    }
+
+    @Test
+    void toValueFromBytes() {
+        byte[] buf = {1, 2, 3, 4, 5};
+        ctx.toValue(buf);
     }
 }
