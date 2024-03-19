2450a397c17f1dd3cda02b854aa80a8a4107c807
Aram Hăvărneanu <aram@mgk.ro>
2024-03-19 12:40:37 +0100

    start adding tests
    Just a stub for now.
    
    Signed-off-by: Aram Hăvărneanu <aram@mgk.ro>

---
+++ src/test/java/org/cuelang/cue/CueContextTest.java
@@ -0,0 +1,19 @@
+package org.cuelang.cue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CueContextTest {
+    CueContext ctx;
+
+    @BeforeEach
+    void setUp() {
+        this.ctx = new CueContext();
+    }
+
+    @Test
+    void top() {
+    }
+}
